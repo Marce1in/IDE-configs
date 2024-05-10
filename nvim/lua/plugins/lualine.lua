2c4616793416a6265f55ab512d7a1672396b1fc3
Marcelo <marcelo.desenvolvedor.contato@gmail.com>
2024-05-10 17:18:01 -0300

    updating colorscheme and some configs

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,9 +2,24 @@ return {
     'nvim-lualine/lualine.nvim',
     opts = {
         options = {
-            theme = 'onedark',
-            componet_separators = '|',
-            section_separators = ' ',
+            theme = (
+                function ()
+                    local t = require('lualine.themes.codedark')
+                    t.normal.c.bg = '#101010'
+                    t.normal.b.bg = '#101010'
+
+                    t.visual.b.bg = '#101010'
+
+                    t.replace.c.bg = '#101010'
+                    t.replace.b.bg = '#101010'
+
+                    t.insert.b.bg = '#101010'
+                    t.insert.c.bg = '#101010'
+
+                    t.inactive.b.bg = '#101010'
+                    return t
+                end
+            ),
         }
     }
 }
--- nvim/lua/plugins/one-dark-colorscheme.lua
+++ nvim/lua/plugins/one-dark-colorscheme.lua
@@ -5,6 +5,15 @@ return {
     priority = 1000,
     opts = {
       style = "warmer",
+      colors = {
+        bg0 = '#000000',
+        bg1 = '#202020',
+        bg2 = '#202020',
+        bg3 = '#202020',
+		-- bg_d = "#000000",
+		-- bg_blue = "#000000",
+		-- bg_yellow = "#000000",
+      },
       highlights = {
         -- ["@lsp.type.variable"] = {fg = '#b3e4ff'},
         -- ["@lsp.type.variable"] = {fg = '#a5cfe6'},
